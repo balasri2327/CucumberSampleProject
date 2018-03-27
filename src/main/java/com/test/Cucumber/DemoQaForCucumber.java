c81bf94d56c5c3c3b6c37aec8081c5e96507a79d
balasri <bala23december@gmail.com>
2018-03-27 12:30:15 +0530

    First Commit

---
+++ src/main/java/com/test/Cucumber/DemoQaForCucumber.java
@@ -0,0 +1,51 @@
+package com.test.Cucumber;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class DemoQaForCucumber {
+WebDriver driver;
+WebElement firstName,lastName;
+@Given("^The user is in DemoQa registration page\\.$")
+public void the_user_is_in_DemoQa_registration_page() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+	System.setProperty("webdriver.chrome.driver", "F:\\eclipse\\Sample\\driver\\chromedriver.exe");
+    driver= new ChromeDriver();
+	driver.get("http://demoqa.com/registration/");
+
+}
+
+@When("^The user enter firstname and lastname$")
+public void the_user_enter_firstname_and_lastname() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+	firstName=driver.findElement(By.id("name_3_firstname"));
+	firstName.sendKeys("Bala");
+	lastName=driver.findElement(By.id("name_3_lastname"));
+	lastName.sendKeys("sri");
+}
+
+@Then("^The user verify the entered text in firstname and lastname field$")
+public void the_user_verify_the_entered_text_in_firstname_and_lastname_field() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    Assert.assertEquals("Bala", firstName.getAttribute("type"));
+    Assert.assertEquals("sri", lastName.getAttribute("type"));
+    Assert.assertEquals("Bala", firstName.getText());
+
+}
+
+}
+
+
+
+
+
+
+
+
---
+++ target/classes/com/test/Cucumber/test.feature
@@ -0,0 +1,26 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+@tag
+Feature: Verify the Functionality DemoQa registration page
+
+  @tag1
+  Scenario: Verify firstname and lastname
+    Given The user is in DemoQa registration page.
+    When The user enter firstname and lastname
+    Then The user verify the entered text in firstname and lastname field
